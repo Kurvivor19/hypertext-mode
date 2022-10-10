62c5eae21e0a4980c7b58cb6b73deec6ab69c298
Ivan <kurvivor@gmail.com>
2022-10-10 23:29:44 +0300

    added initial version for adding of org files in the project

--- hypertext-mode.el
+++ hypertext-mode.el
@@ -55,8 +55,10 @@ If values are not present in cache, nothing is saved"
   (let* ((real-root (or root (gethash default-directory hypertext-directories-cache)))
          (real-context (or context (gethash real-root hypertext-context-cache))))
     (if (and real-root real-context)
-        (with-temp-file (expand-file-name hypertext-data-file-name real-root)
-          (insert (let (print-length) (prin1-to-string real-context))))
+        (progn
+          (with-temp-file (expand-file-name hypertext-data-file-name real-root)
+            (insert (let (print-length) (prin1-to-string real-context))))
+          (puthash root context hypertext-context-cache))
       (message "No context found for %s" real-root))))
 
 (defun hypertext-load-context (root)
@@ -71,9 +73,33 @@ If values are not present in cache, nothing is saved"
           ;; lisp data structures
           (puthash root (read (buffer-string)) hypertext-context-cache))))))
 
+(defun hypertext-create-new-context ()
+  "Create new context and place it in the current directory
+Context is a plist with following symbols:
+:author, :project-name, :description, :created, :lastentry, :file-index
+:lastentry is a running index of the nodes for persistent hyperlinks
+:file-index contains alist of form (file dir (links)) for files in project"
+  (hpe/hypertext-edit-properties
+   ((:author "Author")
+    (:email "Email")
+    (:project-name "Project name")
+    (:description "Description"))
+   ;; outside variables are no longer available
+   (let ((root (hypertext-find-root-cache))
+         (new-context `(:author ,:author
+                        :email ,:email
+                        :project-name ,:project-name
+                        :description ,:description
+                        :created ,(format-time-string "%F %R" (current-time))
+                        :lastentry 0
+                        :file-index '())))
+     (puthash root new-context hypertext-context-cache)
+     (hypertext-save-context root new-context))))
+
 (defun hypertext-activate ()
-  "Entry function
-Finds and loads specific values for this directory"
+  "Activete hypertext mode context
+Finds and loads specific context for this directory or creates a new one if necessary.
+See also 'hypertext-create-new-context'."
   (interactive)
   ;; after this call we definitely have something in that folder
   (when (null hypertext-directories-cache)
@@ -83,26 +109,36 @@ Finds and loads specific values for this directory"
   (let ((root (hypertext-find-root-cache)))
     (unless (gethash root hypertext-context-cache)
       (unless (hypertext-load-context root)
-        (hpe/hypertext-edit-properties
-         ((:author "Author")
-          (:project_name "Project name")
-          (:description "Description"))
-         ;; outside variables are no longer available
-         (let ((root (hypertext-find-root-cache))
-               (new-context `((:author . ,:author)
-                              (:project_name . ,:project_name)
-                              (:description . ,:description)
-                              (:created . ,(format-time-string "%F %R" (current-time)))
-                              (:lastentry . 0))))
-            (puthash root new-context hypertext-context-cache)
-            (hypertext-save-context root new-context)))))))
+        (hypertext-create-new-context)))))
 
 (defmacro hypertext-with-context (&rest body)
+  "Execute BODY with hypertext-mode context if there is one present.
+See 'hypertext-create-new-context' for context contents"
   `(let* ((root (hypertext-find-root-cache))
           (context (gethash root hypertext-context-cache)))
      (if (null context)
          (message "No context available")
-       ,@body)))
+       ,@body
+       (hypertext-save-context root context))))
+
+(defun hypertext-add-org-file (file)
+  "Add new file to project"
+  (interactive "FNew org file name:")
+  (hypertext-with-context
+   (when (not (file-exists-p file))
+     ;; make some initial file content
+     (let* ((title-hint (file-name-base file))
+            (title (read-from-minibuffer "Document title: " title-hint)))
+       (with-temp-file file
+         (insert (format "#+TITLE: %s\n" title))
+         (insert (format "#+AUTHOR: %s\n" (plist-get context :author)))
+         (insert (format "#+EMAIL: %s\n" (plist-get context :email)))
+         (insert (format "#+DATE: %s\n" (format-time-string "%F %R" (current-time)))))))
+   (let ((normal-file-name (file-relative-name (expand-file-name file) root))
+         (index (plist-get context :file-index)))
+     (unless (assq normal-file-name index)
+       (plist-put context :file-index (cons (list normal-file-name root nil) index)))
+     (find-file file))))
 
 (defun hypertext-insert-new-node ()
   "Insert new org node into org document"
